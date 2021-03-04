9a445602554988252d083a2df5e62f254c7f6aee
mariussd <marius.s.dreyer@gmail.com>
2021-03-04 13:07:51 +0100

    ouf this isnt working

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,43 @@
 const Sandbox = require("sandbox");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+const corsOptions = {
+    origin: '*'
+};
 
 let s = new Sandbox(); 
 
 let code = "";
 
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-);
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 
-app.post('/', (req, res) => {
-    console.log(req.body.code);
-
-    code = req.body.code;
-
-    let result = runCode();
-
-    res.body = result;
+app.get('/', (req, res) => {
     res.status = 200;
+    res.send('Lol2k');
+})
+
+app.post('/', cors(corsOptions), (req, res) => {
+    console.log('received code:', req.body.code);
+
+    const body = req.body;
+
+    console.log('body:', body)
+
+    code = body.code;
+
+    s.run(code, function(output) {
+        console.log(output);
+
+        res.status(200).send(output.result);
+    })
 })
 
 app.listen(port, () => {
     console.log(`backend listening at http://localhost:${port}`)
 })
-
-
-function runCode() {
-    let result = "";
-
-    s.run(code, function(output) {
-        console.log(output.result);
-        result = output.result;
-    })
-
-    return result;
-}
-
 
--- main.js
+++ main.js
@@ -3,24 +3,35 @@ function callback(result) {
     document.getElementById("result").innerHTML = result;
 }
 
-document.getElementById("submit-button").addEventListener("click", () => {
+document.getElementById("submit-button").addEventListener("click", async () => {
     const code = document.getElementById("code-input").value;
 
-    let xhr = new XMLHttpRequest();
-    console.log('sender kode ' + code)
+    const data = {
+        task: 1,
+        code: code
+    };
+
+    console.log('trying to send code:', JSON.stringify(data));
     
-    xhr.onreadystatechange = function() {
-        console.log('lol2k');
-        console.log(this.readyState)
-        console.log(this.responseText)
-        if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("result").innerHTML = this.responseText;
-        }
-      }
-      
-    xhr.open("POST", "http://localhost:3000", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(`code=${code}`)
+    const result = await fetch('http://localhost:3000', {
+        method: 'POST',
+        origin: 'http://localhost',
+        mode: 'no-cors',
+        body: `task=${data.task}&code=${data.code}`,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    })
+    .then((res) => {
+        res.text();
+        console.log(res);
+    })
+    .then((data) => {
+        console.log(data);
+        return data;
+    });
+
+    document.getElementById("result").innerHTML = result;
 
 });
 
