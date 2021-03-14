24cb2703d6f5322cb4af2029c93f46dce9a3b049
mariussd <marius.s.dreyer@gmail.com>
2021-03-14 19:40:12 +0100

    add evaluation criteria as percentage

--- backend/vmServer.js
+++ backend/vmServer.js
@@ -18,14 +18,75 @@ app.use(
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.status(200).send('lol2k');
+})
+
 app.post('/', (req, res) => {
     const code = req.body.code;
-    const context = vm.createContext();
-    const result = vm.runInContext(code, context);
 
+    // should have a type
+    // it's very big now, but that's just to illustrate how it can be done
+    const testCases = [
+        { 
+            variables: {
+                n: 12 
+            },
+            desiredOutput: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        },
+        { 
+            variables: {
+                n: 0 
+            },
+            desiredOutput: []
+        },
+        { 
+            variables: {
+                n: 2 
+            },
+            desiredOutput: [0, 1]
+        },
+        { 
+            variables: {
+                n: 1 
+            },
+            desiredOutput: [0]
+        },
+        { 
+            variables: {
+                n: 8 
+            },
+            desiredOutput: [0, 1, 1, 2, 3, 5, 8, 13]
+        },
+    ];
+
+    // results and reports are mostly for testing and logging purposes
+    // the user should only see their score
+    const results = [];
+    const reports = [];
+    let successfulTests = 0;
+
+    testCases.forEach((testCase, i) => {
+        const context = vm.createContext(testCase.variables);
+        const result = vm.runInContext(code, context);
+        const report = result.every((item, j) => item === testCase.desiredOutput[j]);
+
+        if (report) {
+            successfulTests += 1;
+        }
+
+        results.push(result);
+        reports.push({
+            [i]: report
+        })
+    })
+    
+    console.log('results:', results);
+    console.log('reports:', reports);
+    
     res.status(201).send({
-        result: result,
-        msg: result
+        result: results,
+        msg: (successfulTests / reports.length) * 100
     });
 })
 
@@ -47,5 +108,4 @@ app.post('/script', (req, res) => {
 app.listen(port, () => {
     console.log(`backend listening at http://localhost:${port}`)
 })
-
 
