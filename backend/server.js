befcb3b33a596653d1668bfd35ae7bf85ced89da
asmundh <aasmund.haugse@gmail.com>
2021-03-18 22:22:52 +0100

    VM2 support for running code

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,62 @@
-const Sandbox = require("sandbox");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import vm from 'vm';
+import { VM } from 'vm2';
+import cors from 'cors';
+import express from 'express';
+import adminRouter from './routes/adminRouter.js';
+import competitionsRouter from './routes/competitionsRouter.js';
+import connectToMongoDb from './mongo.mjs';
 
 const app = express();
-const port = 3000;
-const corsOptions = {
-    origin: '*'
-};
+const port = process.env.PORT || 3000;
 
-let s = new Sandbox(); 
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
-let code = "";
-
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.status = 200;
-    res.send('Lol2k');
-})
+app.use('/admin', adminRouter);
+app.use('/competitions', competitionsRouter);
+connectToMongoDb(app);
 
-app.post('/', cors(corsOptions), (req, res) => {
-    console.log('received code:', req.body.code);
+app.post('/', (req, res) => {
+  const { code } = req.body;
+  const context = vm.createContext({
+    x: 200,
+  });
+  const result = vm.runInContext(code, context);
 
-    const body = req.body;
+  res.status(201).send({
+    result: { oppgave1: result, oppgave2: 'wrong' },
+    msg: result,
+  });
+});
 
-    console.log('body:', body)
+app.post('/script', (req, res) => {
+  /*
+    Uses vm2 instead of vm. Supposedly more secure: https://odino.org/eval-no-more-understanding-vm-vm2-nodejs/
+    */
+  const vm2 = new VM();
+  const preExistingCode = 'x = 200';
+  const { code } = req.body;
+  let stacktrace = null;
+  let result = null;
+  try {
+    vm2.run(preExistingCode);
+    result = vm2.run(code);
+  } catch (e) {
+    stacktrace = e;
+  }
 
-    code = body.code;
-
-    s.run(code, function(output) {
-        console.log(output);
-
-        res.status(200).send(output.result);
-    })
-})
+  res.status(200).send({
+    result: `RES: ${result} ----- msg: ${stacktrace.toString()}`,
+    msg: stacktrace.toString(),
+  });
+});
 
 app.listen(port, () => {
-    console.log(`backend listening at http://localhost:${port}`)
-})
-
+  console.log(`backend listening at http://localhost:${port}`);
+});
