b7eacb6c3e632cff0134f9a0374c8e5513f8c0d2
asmundh <aasmund.haugse@gmail.com>
2021-03-18 22:21:42 +0100

    Add admin and competition routing

--- backend/routes/competitionsRouter.js
+++ backend/routes/competitionsRouter.js
@@ -1,49 +1,79 @@
 import express from 'express';
-import CompetitionModel from '../models/competitionModel';
-import TaskModel from '../models/taskModel';
+import { VM } from 'vm2';
+import CompetitionModel from '../models/competitionModel.js';
+import TaskModel from '../models/taskModel.js';
+import { runTaskCode } from '../controllers/nutController.js';
 
 const router = express.Router();
 
-router.get('/competitions', (req, res) => {
+// const endpoint = 'competitions';
+
+router.get('/', (req, res) => {
   CompetitionModel.find({})
-    .select('name, activeFrom activeTo image')
-    .sort('-activeTo')
-    .then(competitions => res.json(competitions))
-    .catch(() => res.status(400).send("Something went wrong."))
+    .select('name isActive image')
+    .then((competitions) => res.json(competitions))
+    .catch(() => res.status(400).send('Something went wrong.'));
 });
 
-router.get('/competitions/:name', (req, res) => {
-  const {name} = req.params;
+router.get('/:name', (req, res) => {
+  const { name } = req.params;
 
-  CompetitionModel.find({name: name})
+  CompetitionModel.find({ nameNormalized: name })
     .select('name, activeFrom activeTo image tasks')
     .populate('tasks', 'name description image prize')
-    .then(tasks => res.json(tasks))
-    .catch(() => res.status(400).send("Something went wrong."))
+    .then((tasks) => res.json(tasks))
+    .catch(() => res.status(400).send('Something went wrong.'));
 });
 
-router.get('/competitions/:name/', (req, res) => {
-  const {name} = req.params;
+router.get('/:name/', (req, res) => {
+  const { name } = req.params;
 
-  CompetitionModel.find({name: name})
+  CompetitionModel.find({ nameNormalized: name })
     .select('name, tasks')
     .populate('tasks', 'name description image prize')
     .sort('-day')
-    .then(tasks => res.json(tasks))
-    .catch(() => res.status(400).send("Something went wrong."))
+    .then((tasks) => res.json(tasks))
+    .catch(() => res.status(400).send('Something went wrong.'));
 });
 
-router.get('/competitions/:name/day/:day', (req, res) => {
-  const {day, name} = req.params;
+router.get('/:name/day/:day', (req, res) => {
+  const { day, name } = req.params;
 
-  CompetitionModel.find({name: name})
+  CompetitionModel.find({ nameNormalized: name })
     .select('name, tasks')
     .populate({
       path: 'tasks',
-      match: {day: parseInt(day, 10)},
-      select: 'name description image prize subtasks'
+      match: { day: parseInt(day, 10) },
+      select: 'name description image prize subtasks',
     })
-    .then(tasks => res.json(tasks))
-    .catch(() => res.status(400).send("Something went wrong."))
+    .then((tasks) => res.json(tasks))
+    .catch(() => res.status(400).send('Something went wrong.'));
 });
 
+router.post('/nuts/:competition/:nut', async (req, res) => {
+  console.log('FIRE');
+  const { code } = req.body;
+  const task = await runTaskCode(req, res);
+  const { context, prependedCode, appendedCode } = task;
+
+  const vm = new VM();
+
+  let stacktrace = '';
+  let result = null;
+  try {
+    vm.run(prependedCode);
+    result = vm.run(code);
+    vm.run(appendedCode);
+  } catch (e) {
+    stacktrace = e;
+  }
+
+  res.status(200).send({
+    result: `RES: ${result} ----- msg: ${stacktrace.toString()}`,
+    msg: stacktrace.toString(),
+  });
+});
+
+const competitionsRouter = router;
+
+export default competitionsRouter;
