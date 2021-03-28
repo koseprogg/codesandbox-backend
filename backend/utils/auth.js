d33f3c1d6526c6eff4a6d871dab1b91e506043ae
Ludvig Hozman <ludvig123@gmail.com>
2021-03-28 15:12:43 +0200

    Maybe fix the auth middleware

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 const jwtAuth = (req, res, next) => {
   if (req.headers.authorization) {
     const [, token] = req.headers.authorization.split(' ');
-    if (!token) return next();
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-      if (err) return next();
-      req.user = user;
-    });
+    if (token) {
+      jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (!err) req.user = user;
+      });
+    }
   }
-  return next();
+  next();
 };
 
 module.exports = { jwtAuth };
